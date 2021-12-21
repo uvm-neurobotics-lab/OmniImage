d50e307756e2798b63cc489cca985d167f5d73a7
lfrati <lfrati@uvm.edu>
2021-12-21 11:00:53 -0500

    Improve evolutionary alg.

--- evolve.py
+++ evolve.py
@@ -14,7 +14,7 @@ from utils.image import class2paths
 def subset(matrix, cols):
     """
     Optimized version of np.ix_ for symmetric mat with zero diag
-    
+
     N,C = 5,3 # N size of matrix, C columns selected
     m = np.random.rand(N, N)
     m = (m + m.T)/2 # symmetric
@@ -23,7 +23,7 @@ def subset(matrix, cols):
     sub = m[np.ix_(select, select)]
     triu = np.triu_indices(len(sub), k=1)
     assert np.allclose(sub[triu], subset(m, select))
-    
+
     e.g. N = 5, C = 3
     m:
           | 0  a  b  c  d |
@@ -31,14 +31,14 @@ def subset(matrix, cols):
           | b  e  0  h  i |
           | c  f  h  0  j |
           | d  g  i  j  0 |
-            
+
     select: [1,3,4]
-    
+
     sub:
           | 0  f  g |
           | f  0  j |
           | g  j  0 |
-            
+
     sub[triu]: [f, g, j] # len = 3*(3-1)//2
     """
     C = cols.shape[0]
@@ -52,10 +52,11 @@ def subset(matrix, cols):
 
 
 @njit
-def mutate(parent, space, loci, num_muts=2):
-    valid = np.array(list(space - set(parent)))
-    mutations = choice(valid, size=num_muts, replace=False)
-    positions = choice(loci, size=num_muts, replace=False)
+def mutate(parent, valid, loci, M):
+    # how to mutate (only new values)
+    mutations = choice(valid, size=M, replace=False)
+    # where to mutate
+    positions = choice(loci, size=M, replace=False)
     child = np.copy(parent)
     child[positions] = mutations
     return child
@@ -63,76 +64,78 @@ def mutate(parent, space, loci, num_muts=2):
 
 @njit
 def cross(dad, mom):
-    valid = np.array(list(set(dad) | set(mom)))
+    valid = np.array(list(set(dad) | set(mom)))  # cast to np for numba reasons
     child = choice(valid, size=len(dad), replace=False)
     return child
 
 
-# top_k[0], mutate(top_k[0], num_muts)
-# top_k[0], top_k[1], cross(top_k[0], top_k[1])
+def score(pop, distances, inverse):
+    fitnesses = np.array([subset(distances, m).sum() for m in pop])
+    if inverse:
+        return -fitnesses
+    return fitnesses
 
 
 @njit(parallel=True)
-def evolve(top_k, params, space):
-    # k, M = top_k.shape
-    loci = np.arange(params.M)
-    children = np.empty(((params.N - params.k), params.M))
-    # mutate top_k to get children
-    for i in prange(params.N - params.k):
-        children[i] = mutate(top_k[i % params.k], space, loci, params.muts)
-    # cross pairs of children, keep the first one (more fit), update the second
-    for i in range(0, params.N - params.k - 1, 2):
-        children[i + 1] = cross(children[i], children[i + 1])
-    return children
-
-
-def score(pop, distances):
-    # return np.array([distances[np.ix_(m, m)].sum() for m in pop])
-    return np.array([subset(distances, m).sum() for m in pop])
-
-
-def step(pop, fitnesses, params, space):
+def step(pop, fitnesses, P, S, K, M, O, space):
     indexes = np.argsort(fitnesses)
-    good_idxs = indexes[: params.k]
-    bad_idxs = indexes[params.k :]
-    top_k = np.copy(pop[good_idxs])
-    children = evolve(top_k, params, space)
-    pop[bad_idxs] = children
+    fit_idxs = indexes[:K]
+    unfit_idxs = indexes[K:]
+    top_k = pop[fit_idxs]
+    loci = np.arange(S)
+    # compute valid indices to sample from during mutation
+    valid = np.empty((K, len(space) - S))
+    for i in prange(K):
+        valid[i] = np.array(list(space - set(top_k[i])))
+    children = np.empty(((P - K), S))
+
+    ##### MUTATION ######
+    for i in prange(P - K):
+        j = i % K
+        children[i] = mutate(top_k[j], valid[j], loci, M)
+
+    ##### CROSSOVER ######
+    for i in prange(0, P - K - 1):
+        if i % O == 0:  # only cross 1/O of the offspring
+            # children are arranged in sorted bands so fit[i]>fit[i+1] (apart from in between bands)
+            children[i + 1] = cross(children[i], children[i + 1])
+
+    pop[unfit_idxs] = children
+
     return pop
 
 
-def select_best(distances, params, inverse=False, progress=False):
-
-    space = set(np.arange(distances.shape[0]))
-    pop = np.array(
-        [
-            np.sort(choice(list(space), size=params.M, replace=False))
-            for _ in range(params.N)
-        ]
-    )
+def select_best(distances, P, S, K, M, O, its, inverse=False, progress=False):
+    # distances : [N,L] N vectors of length L
+    # P : population size
+    # S : subset size
+    # K : number of parents (P-K children)
+    # M : number of mutations
+    # O : fraction of crossovers e.g. O=2 -> 1/2, O=10 -> 1/10, (bigger=faster)
+    N = distances.shape[0]
+    assert S <= N, f"Error: value of S={S} is > than the #distances={N}"
+    assert O >= 1, f"Error: value of O={S} is < 1"
+    space = set(range(N))
+    pop = np.array([np.arange(S) for _ in range(P)])
+    fitnesses = np.zeros(P)
 
     stats = defaultdict(list)
     range_fun = trange if progress else range
-    for _ in range_fun(params.its):
-        fitnesses = score(pop, distances)
-        if inverse:
-            fitnesses *= -1
+    for _ in range_fun(its):
 
-        best = pop[np.argmin(fitnesses)].copy()
-        stats["top_dogs"].append(best)
+        pop = step(pop, fitnesses, P, S, K, M, O, space)
+
+        fitnesses = score(pop, distances, inverse)
+
+        best = np.copy(pop[np.argmin(fitnesses)])
+        stats["bests"].append(best)
         stats["fits"].append(fitnesses.min())
 
-        pop = step(pop, fitnesses, params, space)
-
-    fitnesses = score(pop, distances)
-    if inverse:
-        fitnesses *= -1
-    best = pop[np.argmin(fitnesses)].copy()
+    fitnesses = score(pop, distances, inverse)
+    best = np.copy(pop[np.argmin(fitnesses)])
 
     return best, stats
 
-
-Params = namedtuple("Params", "N M k muts its")
 
 #%%
 
@@ -149,25 +152,24 @@ if __name__ == "__main__":
     # N = 1000  # population size
     # M = 20  # dna size
     # k = 200  # how many get to fuck (out of N)
-    # muts = M // 4
-    params = Params(N=1000, M=20, k=100, muts=2, its=2_000)
-    best, stats = select_best(distances, params, progress=True)
-
+    best, stats = select_best(
+        distances, P=1000, S=20, K=100, M=2, O=2, its=2_000, progress=True
+    )
     print("Final fit:", stats["fits"][-1])
 
-    show_evolution(cls, distances, stats)
+    # show_evolution(cls, distances, stats)
 
     # REPEAT EVOLUTION AFTER REMOVING THE TOP DOGS
     # refined = np.array(
-    #     list(set(np.arange(len(distances))) - set(stats["top_dogs"][-1]))
+    #     list(set(np.arange(len(distances))) - set(stats["bests"][-1]))
     # )
     # distances2 = distances[np.ix_(refined, refined)]
     # best2, stats2 = select_best(distances2, params, progress=True)
     # show_evolution(distances2, stats2)
-    # candidates = np.array(stats["top_dogs"])
+    # candidates = np.array(stats["bests"])
     # genotype_over_time(candidates)
     # # steps at which fitness change
     # improvements = improvements_only(stats["fits"])
     # t = np.array([i[0] for i in improvements])
-    # candidates = np.array(stats["top_dogs"])[t]
+    # candidates = np.array(stats["bests"])[t]
     # genotype_over_time(candidates)
