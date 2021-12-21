37e9af5c3c8fe9be58cd3b48e3f02be432a2c635
lfrati <lfrati@uvm.edu>
2021-12-21 13:17:25 -0500

    Improve sampler.

--- dataset.py
+++ dataset.py
@@ -72,10 +72,15 @@ def is_sorted(l):
 
 
 class OMLSampler:
-    def __init__(self, dataset, nsamples_per_class=20):
+    def __init__(self, dataset, device="cpu", nsamples_per_class=20):
         # assumes indexes are sorted per class 00..0011..1122...
         # assert is_sorted([dataset[i][1] for i in range(len(dataset))])
         self.dataset = dataset
+        self.device = device
+        assert self.device in [
+            "cpu",
+            "cuda",
+        ], f"Device must be either 'cpu' or 'cuda': {device} found."
         self.samples = np.arange(len(dataset))
         self.class_idxs = self.samples.reshape(-1, nsamples_per_class)
         self.classes = np.arange(self.class_idxs.shape[0])
@@ -85,30 +90,33 @@ class OMLSampler:
         self.slice = 0
 
     def __repr__(self):
-        return f"Sampler: {len(self.dataset)} SAMPLES, {len(self.classes)} CLASSES"
-    
+        return f"Sampler: {len(self.dataset)} SAMPLES, {len(self.classes)} CLASSES, DEVICE={self.device}"
+
     def get(self, idxs):
         # get batch of ims,labels from a list of indices
         ims = [self.dataset[i][0] for i in idxs]
         lbs = [self.dataset[i][1] for i in idxs]
-        return torch.stack(ims), torch.tensor(lbs)
+        return torch.stack(ims).to(self.device), torch.tensor(lbs).to(self.device)
 
-    def sample_class(self):
-        # get all 20 ims of a single class, used for inner loop
+    def sample_class(self, N=20):
+        # get N ims of a single class, used for inner loop
+        # NOTE: always gets the same N ims
         cls = np.random.choice(self.classes)
-        return self.get(self.class_idxs[cls])
+        return self.get(self.class_idxs[cls][:N])
 
-    def sample_random(self, sample_size=64):
-        # get a random sample from the whole dataset, used for the outer loop
-        samples = np.random.choice(self.samples, size=sample_size, replace=False)
+    def sample_random(self, N=64):
+        # get N at random from the whole dataset, used for the outer loop
+        samples = np.random.choice(self.samples, size=N, replace=False)
         return self.get(samples)
 
-    def sample(self, sample_size=64):
+    def sample(self, inner_size=20, outer_size=64, expand=True):
         # get 20 ims for the inner and 20+64 ims for the outer
-        inner_ims, inner_labels = self.sample_class()
-        outer_ims, outer_labels = self.sample_random(sample_size)
+        inner_ims, inner_labels = self.sample_class(inner_size)
+        outer_ims, outer_labels = self.sample_random(outer_size)
         outer_ims = torch.cat([inner_ims, outer_ims])
         outer_labels = torch.cat([inner_labels, outer_labels])
+        if expand:
+            inner_ims = inner_ims.unsqueeze(1)
         return inner_ims, inner_labels, outer_ims, outer_labels
 
     def sample_balanced(self):
@@ -133,10 +141,28 @@ if __name__ == "__main__":
     train, test, tr_cls, te_cls = split(dataset, verbose=True)
     print(len(train), len(test))
 
-    sampler = OMLSampler(train)
+    device = "cuda"
+    sampler = OMLSampler(train, device)
     print(sampler)
-    
+
     # OMLSampler(dataset) to use the whole dataset instead
 
+    # inner/outer sample sizes
+    I, O = 15, 32
+    inner_ims, inner_labels, outer_ims, outer_labels = sampler.sample(
+        inner_size=I, outer_size=O
+    )
+    print(inner_ims.shape)
+    print(inner_labels.shape)
+    print(outer_ims.shape)
+    print(outer_labels.shape)
+    assert (
+        inner_ims.shape[0] == I
+    ), f"Inner loop mismatch: Expected {I} found {inner_ims.shape[0]}"
+    assert (
+        outer_ims.shape[0] == I + O
+    ), f"Outer loop mismatch: Expected {I+O} found {outer_ims.shape[0]}"
+
     ims, labels = sampler.sample_balanced()
+    assert ims.device.type == device
     assert len(set([label.item() for label in labels])) == len(labels)
